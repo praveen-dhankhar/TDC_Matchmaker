1d5308d9820ff679283639e66d1f3fd589168a68
praveen-dhankhar <178932339+praveen-dhankhar@users.noreply.github.com>
2026-06-06 09:32:52 +0530

    fixed tsconfig

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -8,7 +8,7 @@ export default tseslint.config(
     files: ['src/**/*.ts'],
     languageOptions: {
       parserOptions: {
-        project: './tsconfig.json',
+        project: './tsconfig.eslint.json',
         tsconfigRootDir: import.meta.dirname,
       },
     },
